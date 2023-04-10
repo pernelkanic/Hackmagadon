e711aeb7af77acb589742eab0e093da5f20cd2f0
pernelkanic <whitehattt03@gmail.com>
2023-04-10 18:30:52 +0530

    sdfaf

---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import styles from "./SignUp.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { notify } from "./toast";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [touched, setTouched] = useState({});
+
+
+
+  const changeHandler = (event) => {
+    if (event.target.name === "IsAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+  };
+
+  const focusHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.Head}>BlockChain Tendering System</h1>
+      <form className={styles.formLogin}  autoComplete="off">
+        <h2>Sign In</h2>
+        <div>
+          <div>
+            <input type="text" name="email" value={data.email} placeholder="E-mail" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            
+          </div>
+        </div>
+        <div>
+          <div>
+            <input type="password" name="password" value={data.password} placeholder="Password" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            
+          </div>
+        </div>
+
+        <div>
+          <button type="submit">Login</button>
+          <span style={{ color: "#a29494", textAlign: "center", display: "inline-block", width: "100%" }}>
+            Don't have a account? <Link to="/signup">Create account</Link>
+          </span>
+        </div>
+      </form>
+      
+    </div>
+  );
+};
+
+export default Login;
